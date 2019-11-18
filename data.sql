389ba98b163b83e52e3317c58e88a5733d1877fa
cxhello <1061594797@qq.com>
2019-11-18 13:59:45 +0800

    sql修改

--- data.sql
+++ data.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 50554
  Source Host           : 192.168.3.128:3306
- Source Schema         : base
+ Source Schema         : springboot_admin
 
  Target Server Type    : MySQL
  Target Server Version : 50554
  File Encoding         : 65001
 
- Date: 14/11/2019 11:20:19
+ Date: 18/11/2019 13:56:32
 */
 
 SET NAMES utf8mb4;
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `tb_resource`;
 CREATE TABLE `tb_resource`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `is_hide` int(11) NULL DEFAULT NULL,
+  `source_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` int(11) NULL DEFAULT NULL,
+  `source_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `level` int(11) NULL DEFAULT NULL,
   `sort` int(11) NULL DEFAULT NULL,
-  `source_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `source_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `type` int(11) NULL DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `is_hide` int(11) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `create_time` datetime NULL DEFAULT NULL,
   `update_time` datetime NULL DEFAULT NULL,
   `parent_id` int(11) NULL DEFAULT NULL,
@@ -41,20 +41,20 @@ CREATE TABLE `tb_resource`  (
 -- ----------------------------
 -- Records of tb_resource
 -- ----------------------------
-INSERT INTO `tb_resource` VALUES (1, '用户管理', '用户管理', NULL, 0, 2, 1, 'system:user:index', '/admin/user/index', 1, '2019-11-10 13:56:51', '2019-11-10 13:59:01', 0);
-INSERT INTO `tb_resource` VALUES (2, '用户编辑', '用户编辑', NULL, 0, 3, 1, 'system:user:edit', '/admin/user/edit*', 2, '2019-11-10 13:56:51', '2019-11-10 16:26:42', 1);
-INSERT INTO `tb_resource` VALUES (3, '用户添加', '用户添加', NULL, 0, 3, 2, 'system:user:add', '/admin/user/add', 2, '2019-11-10 16:48:48', '2019-11-10 16:49:26', 1);
-INSERT INTO `tb_resource` VALUES (4, '用户删除', '用户删除', NULL, 0, 3, 3, 'system:user:deleteBatch', '/admin/user/deleteBatch', 2, '2019-11-10 16:48:48', '2019-11-10 14:11:41', 1);
-INSERT INTO `tb_resource` VALUES (5, '角色分配', '角色分配', NULL, 0, 3, 4, 'system:user:grant', '/admin/user/grant/**', 2, '2019-11-10 16:48:48', '2019-11-10 14:11:51', 1);
-INSERT INTO `tb_resource` VALUES (6, '角色管理', '角色管理', NULL, 0, 2, 2, 'system:role:index', '/admin/role/index', 1, '2019-11-10 16:45:10', '2019-11-10 16:46:52', 0);
-INSERT INTO `tb_resource` VALUES (7, '角色编辑', '角色编辑', NULL, 0, 3, 1, 'system:role:edit', '/admin/role/edit*', 2, '2019-11-10 16:47:02', '2019-11-10 10:24:06', 6);
-INSERT INTO `tb_resource` VALUES (8, '角色添加', '角色添加', NULL, 0, 3, 2, 'system:role:add', '/admin/role/add', 2, '2019-11-10 16:47:23', '2019-11-10 16:49:16', 6);
-INSERT INTO `tb_resource` VALUES (9, '角色删除', '角色删除', NULL, 0, 3, 3, 'system:role:deleteBatch', '/admin/role/deleteBatch', 2, '2019-11-10 16:47:23', '2019-11-10 14:12:03', 6);
-INSERT INTO `tb_resource` VALUES (10, '资源分配', '资源分配', NULL, 0, 3, 4, 'system:role:grant', '/admin/role/grant/**', 2, '2019-11-10 16:47:23', '2019-11-10 14:12:11', 6);
-INSERT INTO `tb_resource` VALUES (11, '资源管理', '资源管理', NULL, 0, 2, 3, 'system:resource:index', '/admin/resource/index', 1, '2019-11-10 11:21:12', '2019-11-10 11:21:42', 0);
-INSERT INTO `tb_resource` VALUES (12, '资源编辑', '资源编辑', NULL, 0, 3, 1, 'system:resource:edit', '/admin/resource/edit*', 2, '2019-11-10 11:21:52', '2019-11-10 11:22:36', 11);
-INSERT INTO `tb_resource` VALUES (13, '资源添加', '资源添加', NULL, 0, 3, 2, 'system:resource:add', '/admin/resource/add', 2, '2019-11-10 11:21:54', '2019-11-10 11:22:39', 11);
-INSERT INTO `tb_resource` VALUES (14, '资源删除', '资源删除', NULL, 0, 3, 3, 'system:resource:deleteBatch', '/admin/resource/deleteBatch', 2, '2019-11-10 11:21:54', '2019-11-10 14:12:31', 11);
+INSERT INTO `tb_resource` VALUES (1, '用户管理', 'system:user:index', 1, '/admin/user/index', 2, 1, NULL, 0, '用户管理', '2019-11-10 13:56:51', '2019-11-10 13:59:01', 0);
+INSERT INTO `tb_resource` VALUES (2, '用户编辑', 'system:user:edit', 2, '/admin/user/edit*', 3, 1, NULL, 0, '用户编辑', '2019-11-10 13:56:51', '2019-11-10 16:26:42', 1);
+INSERT INTO `tb_resource` VALUES (3, '用户添加', 'system:user:add', 2, '/admin/user/add', 3, 2, NULL, 0, '用户添加', '2019-11-10 16:48:48', '2019-11-10 16:49:26', 1);
+INSERT INTO `tb_resource` VALUES (4, '用户删除', 'system:user:deleteBatch', 2, '/admin/user/deleteBatch', 3, 3, NULL, 0, '用户删除', '2019-11-10 16:48:48', '2019-11-10 14:11:41', 1);
+INSERT INTO `tb_resource` VALUES (5, '角色分配', 'system:user:grant', 2, '/admin/user/grant/**', 3, 4, NULL, 0, '角色分配', '2019-11-10 16:48:48', '2019-11-10 14:11:51', 1);
+INSERT INTO `tb_resource` VALUES (6, '角色管理', 'system:role:index', 1, '/admin/role/index', 2, 2, NULL, 0, '角色管理', '2019-11-10 16:45:10', '2019-11-10 16:46:52', 0);
+INSERT INTO `tb_resource` VALUES (7, '角色编辑', 'system:role:edit', 2, '/admin/role/edit*', 3, 1, NULL, 0, '角色编辑', '2019-11-10 16:47:02', '2019-11-10 10:24:06', 6);
+INSERT INTO `tb_resource` VALUES (8, '角色添加', 'system:role:add', 2, '/admin/role/add', 3, 2, NULL, 0, '角色添加', '2019-11-10 16:47:23', '2019-11-10 16:49:16', 6);
+INSERT INTO `tb_resource` VALUES (9, '角色删除', 'system:role:deleteBatch', 2, '/admin/role/deleteBatch', 3, 3, NULL, 0, '角色删除', '2019-11-10 16:47:23', '2019-11-10 14:12:03', 6);
+INSERT INTO `tb_resource` VALUES (10, '资源分配', 'system:role:grant', 2, '/admin/role/grant/**', 3, 4, NULL, 0, '资源分配', '2019-11-10 16:47:23', '2019-11-10 14:12:11', 6);
+INSERT INTO `tb_resource` VALUES (11, '资源管理', 'system:resource:index', 1, '/admin/resource/index', 2, 3, NULL, 0, '资源管理', '2019-11-10 11:21:12', '2019-11-10 11:21:42', 0);
+INSERT INTO `tb_resource` VALUES (12, '资源编辑', 'system:resource:edit', 2, '/admin/resource/edit*', 3, 1, NULL, 0, '资源编辑', '2019-11-10 11:21:52', '2019-11-10 11:22:36', 11);
+INSERT INTO `tb_resource` VALUES (13, '资源添加', 'system:resource:add', 2, '/admin/resource/add', 3, 2, NULL, 0, '资源添加', '2019-11-10 11:21:54', '2019-11-10 11:22:39', 11);
+INSERT INTO `tb_resource` VALUES (14, '资源删除', 'system:resource:deleteBatch', 2, '/admin/resource/deleteBatch', 3, 3, NULL, 0, '资源删除', '2019-11-10 11:21:54', '2019-11-10 14:12:31', 11);
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -113,15 +113,15 @@ CREATE TABLE `tb_user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `nick_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `sex` int(11) NULL DEFAULT NULL,
   `birthday` datetime NULL DEFAULT NULL,
   `telephone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `delete_status` int(11) NULL DEFAULT NULL,
   `locked` int(11) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `create_time` datetime NULL DEFAULT NULL,
   `update_time` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -130,7 +130,7 @@ CREATE TABLE `tb_user`  (
 -- ----------------------------
 -- Records of tb_user
 -- ----------------------------
-INSERT INTO `tb_user` VALUES (1, 'admin', 'admin', 1, '1995-12-13 17:26:39', '17710372161', 'caixiaohuichn@163.com', '北京', '超级管理员', '3931MUEQD1939MQMLM4AISPVNE', 0, 0, '2019-11-10 17:26:41', '2019-11-10 17:27:11');
+INSERT INTO `tb_user` VALUES (1, 'admin', 'admin', '3931MUEQD1939MQMLM4AISPVNE', 1, '1995-12-13 00:00:00', '17710372161', 'caixiaohuichn@163.com', '北京', 0, 0, '超级管理员', '2019-11-10 17:26:41', '2019-11-18 12:49:49');
 
 -- ----------------------------
 -- Table structure for tb_user_role
