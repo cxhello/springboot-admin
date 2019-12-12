ef97efcb6b7a4b0bedaed8c7aa0c8c74c1630a6a
cxhello <1061594797@qq.com>
2019-12-12 20:37:23 +0800

    配置log4j2日志过滤规则

--- src/main/java/com/cxhello/admin/SpringbootAdminApplication.java
+++ src/main/java/com/cxhello/admin/SpringbootAdminApplication.java
@@ -1,5 +1,7 @@
 package com.cxhello.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -8,8 +10,11 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.cxhello.admin.dao")
 public class SpringbootAdminApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(SpringbootAdminApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAdminApplication.class, args);
+        logger.debug("启动成功");
     }
 
 }
